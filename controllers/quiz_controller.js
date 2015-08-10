235a08938e82c53079d1d5571c646d62733a1fc2
JhonCastilloH <profe.johncastillo@gmail.com>
2015-08-09 20:52:21 -0500

    “ListaPreguntas”

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -9,7 +9,6 @@ exports.question= function(req, res){
 
 
 // get quizes / answer
-
 exports.answer= function(req, res){
 	models.Quiz.findAll().success(function(quiz){
 		if (req.query.respuesta===quiz[0].respuesta){
