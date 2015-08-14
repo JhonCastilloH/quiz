e5a2208679b92ba3aa0b84a5ed8404eb81716755
JhonCastilloH <profe.johncastillo@gmail.com>
2015-08-13 19:01:09 -0500

    create y validate

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,26 +12,53 @@ exports.load= function(req, res, next, quizId){
 };
 
 
-// get quizes / question
-exports.index= function(req, res){
-	if (req.query.search) {
-		models.Quiz.findAll({
-			where: ["pregunta like ?", '%' + req.query.search.replace(/\s/g,"%") + '%'],
-			order:[['pregunta','ASC']]
-		}).then(function(quizes){
-		res.render('quizes/index.ejs',{quizes: quizes});
-		});
-		}else{ 
-		models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs',{quizes: quizes});
-	});
+// GET /quizes
+exports.index = function(req, res){
+	if(req.query.search){
+		var search = '%' + req.query.search.replace(/\s/g,"%") + '%';
+
+		models.Quiz.findAll(
+			{where: ["pregunta like ?", search],
+			order:[['pregunta','ASC']]}
+			).then(function(quizes){
+				res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+			}).catch(function(error){next (error);});
+	}else{
+		models.Quiz.findAll().then(function (quizes) {
+			res.render('quizes/index.ejs', { quizes: quizes, errors: []});
+		}).catch(function(error) {next(error);});
 	}
 };
 
+// get quizes / new
+exports.new= function(req, res){
+	var quiz = models.Quiz.build( //crea objeto quiz
+	{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+	res.render('quizes/new',{quiz:quiz, errors: []});
+};
+
+
+// get quizes / create
+exports.create = function(req, res){
+var quiz = models.Quiz.build( req.body.quiz );
+
+var errors = quiz.validate();//ya qe el objeto errors no tiene then(
+if (errors)
+{
+var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+for (var prop in errors) errores[i++]={message: errors[prop]};	
+res.render('quizes/new', {quiz: quiz, errors: errores});
+} else {
+quiz // save: guarda en DB campos pregunta y respuesta de quiz
+.save({fields: ["pregunta", "respuesta"]})
+.then( function(){ res.redirect('/quizes')}) ;
+}
+};
 
 // get quizes / question
 exports.show= function(req, res){
-		res.render('quizes/show',{quiz: req.quiz});
+		res.render('quizes/show',{quiz: req.quiz, errors: []});
 };
 
 
@@ -41,5 +68,5 @@ exports.answer= function(req, res){
 		if (req.query.respuesta===req.quiz.respuesta){
 			resultado='Correcto';
 		}
-			res.render('quizes/answer',{quiz:req.quiz,respuesta:resultado}) 
+			res.render('quizes/answer',{quiz:req.quiz,respuesta:resultado, errors: []}) 
 };
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz'});
+  res.render('index', { title: 'Quiz', errors:[]});
 });
 
 // Autoload de comandos con: quizId
@@ -16,6 +15,8 @@ router.param('quizId', quizController.load);
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 //router.get('/quizes/question',quizController.question);
 //router.get('/quizes/answer',quizController.answer);
